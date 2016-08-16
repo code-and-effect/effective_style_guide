609b0cfae8241cc447f4991a56626bb8e63c2f18
Matt Riemer <matthew@agilestyle.com>
2016-08-16 11:10:27 -0600

    clean up style_guide model

--- app/models/effective/style_guide.rb
+++ app/models/effective/style_guide.rb
@@ -6,29 +6,13 @@ module Effective
       acts_as_asset_box files: 1..6
     end
 
-    attr_accessor :id, :title, :email, :password, :number, :range, :category, :content
-    attr_accessor :archived, :drink, :food, :price, :updated_at, :publish_on, :static_text
+    ATTRIBUTES = [
+      :id, :title, :email, :password, :number, :range, :category, :content,
+      :archived, :drink, :food, :price, :updated_at, :publish_on, :static_text
+    ]
 
-
-    # column :id, :integer
-
-    # column :title, :string
-    # column :email, :string
-    # column :password, :string
-    # column :number, :integer
-    # column :range, :integer
-    # column :category, :string
-    # column :content, :text
-    # column :archived, :boolean
-    # column :drink, :string
-    # column :food, :string
-    # column :price, :integer
-    # column :updated_at, :datetime
-    # column :publish_on, :date
-    # column :static_text, :string
-
-    validates :id, :title, :email, :password, :number, :range, :category, :content, presence: true
-    validates :archived, :drink, :food, :price, :updated_at, :publish_on, :static_text, presence: true
+    attr_accessor *ATTRIBUTES
+    validates *ATTRIBUTES, presence: true
 
     def static_text
       'some static text'
